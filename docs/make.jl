574f371cb32e48db3d8f1a1ab199b03ee0e050a9
Trym Hamer Gudvangen <trym.gudvangen@gmail.com>
2025-02-14 00:35:51 +0100

    docs: add auto-deploy to GH pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,7 @@
 using Documenter, MutatedNurses
 
 makedocs(sitename="MutatedNurses")
+deploydocs(
+    repo = "github.com/TrymNOHG/mutated-nurses.git",
+    push_preview = true # Optional: Preview before merging
+)
